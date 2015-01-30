5ff100b3efaec35514ce8d9ea2dc1ba841960e29
George <george.olaru@pixelgrade.com>
2015-01-30 16:25:53 +0200

    +

--- +production.rb
+++ +production.rb
@@ -3,4 +3,4 @@
 # change to script
 Dir.chdir File.expand_path(File.dirname(__FILE__))
 # run compass compiler
-Kernel.exec('sass --compass --force --update scss:css -E utf-8 2> /dev/null')
+Kernel.exec('sass --compass --force --update scss:css --style expanded -E utf-8 2> /dev/null')
