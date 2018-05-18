edda1fff43201b602a37516bba536cc0a2044e2c
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-05-18 17:07:38 +0300

    cleanup

--- Mockup Font Palettes/theme-config-hive.php
+++ Mockup Font Palettes/theme-config-hive.php
@@ -2,49 +2,52 @@
 
 // HIVE
 // Fonts
+$config = [
+	// Main Content
+	'main_content' => [
+		'options' => [
 
-// Main Content
-'main_content' => (
-	'options' => (
+			// [Section] FONTS
 
-		// [Section] FONTS
-		
-		// Page Title
-		'main_content_page_title_font' => (
-			// Default options used as fallback	
-			'default' => (
-				'font-family'    => 'Playfair Display',
-				'font-weight'    => '400',
-				'font-size'      => 54,
-				'line-height'    => 1.11,
-				'letter-spacing' => 0,
-				'text-transform' => 'none',
-			),
-			'selectors' 	=> (),
+			// Page Title
+			'main_content_page_title_font' => [
+				// Default options used as fallback
+				'default'      => [
+					'font-family'    => 'Playfair Display',
+					'font-weight'    => '400',
+					'font-size'      => 54,
+					'line-height'    => 1.11,
+					'letter-spacing' => 0,
+					'text-transform' => 'none',
+				],
+				'selectors'    => [],
 
-			// Font Palette Rules
-			'font-palette' 	=> (
-				'font' 	=> 'font_primary',
-				'size'	=> 54
-			),
-		),
+				// Font Palette Rules
+				'font-palette' => [
+					'font' => 'font_primary',
+					'size' => 54,
+				],
+			],
 
-		// Heading 3	
-		'main_content_heading_3_font' => (
-			// Default options used as fallback	
-			'default' => (
-				'font-family'    => 'Playfair Display',
-				'font-weight'    => '400',
-				'font-size'      => 27,
-				'line-height'    => 1.5,
-				'letter-spacing' => 0,
-				'text-transform' => 'none',
-			),
-			'selectors' 	=> (),
+			// Heading 3
+			'main_content_heading_3_font'  => [
+				// Default options used as fallback
+				'default'      => [
+					'font-family'    => 'Playfair Display',
+					'font-weight'    => '400',
+					'font-size'      => 27,
+					'line-height'    => 1.5,
+					'letter-spacing' => 0,
+					'text-transform' => 'none',
+				],
+				'selectors'    => [],
 
-			// Font Palette Rules
-			'font-palette' 	=> (
-				'font' 	=> 'font_primary',
-				'size'	=> 27
-			),
-		),
+				// Font Palette Rules
+				'font-palette' => [
+					'font' => 'font_primary',
+					'size' => 27,
+				],
+			],
+		],
+	],
+];
