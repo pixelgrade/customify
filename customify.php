28952ad43f4c8e76b6dadfbff17f895c40519297
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-08-23 11:37:17 +0300

    version bump to 2.0.1

--- customify.php
+++ customify.php
@@ -3,7 +3,7 @@
 Plugin Name: Customify
 Plugin URI:  https://wordpress.org/plugins/customify/
 Description: A Theme Customizer Booster
-Version: 2.0.0
+Version: 2.0.1
 Author: Pixelgrade
 Author URI: https://pixelgrade.com
 Author Email: contact@pixelgrade.com
@@ -61,7 +61,7 @@ function PixCustomifyPlugin() {
 	 */
 	require_once plugin_dir_path( __FILE__ ) . 'class-pixcustomify.php';
 
-	$instance = PixCustomifyPlugin::instance( __FILE__, '2.0.0' );
+	$instance = PixCustomifyPlugin::instance( __FILE__, '2.0.1' );
 
 	return $instance;
 }
