940879599ab6f830f82a8dfcb31f03c4e46a65d6
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-08-30 15:51:25 +0300

    Version bump to 2.5.1

--- customify.php
+++ customify.php
@@ -3,7 +3,7 @@
  * Plugin Name: Customify
  * Plugin URI:  https://wordpress.org/plugins/customify/
  * Description: A Theme Customizer Booster to easily and consistently customize Fonts, Colors, and other options for your site.
- * Version: 2.5.0
+ * Version: 2.5.1
  * Author: Pixelgrade
  * Author URI: https://pixelgrade.com
  * Author Email: contact@pixelgrade.com
@@ -32,7 +32,7 @@ require_once 'includes/extras.php';
 function PixCustomifyPlugin() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-pixcustomify.php';
 
-	return PixCustomifyPlugin::instance( __FILE__, '2.5.0' );
+	return PixCustomifyPlugin::instance( __FILE__, '2.5.1' );
 }
 
 // Now get the party started
