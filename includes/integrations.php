9d0ba22bd4d42a204ce2d1a01bcaf7d111cf48ef
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-03-06 12:40:11 +0200

    Better cache invalidation - see #193

---
+++ includes/integrations.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This is logic for integrating with the Pixelgrade Care plugin.
+ *
+ * @see         https://pixelgrade.com
+ * @author      Pixelgrade
+ * @since       2.6.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
