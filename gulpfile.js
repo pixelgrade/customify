9875db0b3f3b35a9e76fbfb4eb87bcdeb092f8e3
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-03-27 15:33:56 +0200

    Moved Google fonts list update to a Gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,64 @@ var plugin = 'customify',
   cleanCSS = require('gulp-clean-css'),
   commandExistsSync = require('command-exists').sync;
 
-var u = plugins.util,
-  c = plugins.util.colors,
-  log = plugins.util.log;
-
 require('es6-promise').polyfill();
 
-// styles related
+// -----------------------------------------------------------------------------
+// This is a helper function used to update the Google fonts list and recreate the php file holding it (includes/resources/google.fonts.php).
+// -----------------------------------------------------------------------------
+function updatePhpGoogleFontsList(done) {
+  const endpoint = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyB7Yj842mK5ogSiDa3eRrZUIPTzgiGopls';
+  require('request').get({
+    url: endpoint,
+  }, function(error, response, body) {
+    if (error || !body) throw error
+
+    body = JSON.parse(body);
+
+    //require('fs').writeFileSync('post.json', JSON.stringify(body))
+
+    // First lets inspect the body and make sure it is something we can manage.
+    if ( typeof body.items === 'undefined' ) {
+      log.error( 'There was no `items` entry in the response from Google apis. Please manually check the response from `'+endpoint+'`');
+      return done();
+    }
+
+    let fontsList = {};
+    // Go through the items and clean it up to our liking.
+    body.items.map( function(item){
+      delete item.kind;
+      delete item.version;
+      delete item.lastModified;
+      delete item.files;
+
+      fontsList[item.family] = item;
+    })
+
+    // Start the PHP code list.
+    let php = ['<?php'];
+
+    php.push( '// Returns an associative array with fonts.' );
+    php.push( 'return json_decode( \'' + JSON.stringify( fontsList ) + '\', true );' );
+
+    require('fs').writeFileSync('includes/resources/google.fonts.php', php.join( '\r\n' ));
+
+    return done();
+  })
+}
+updatePhpGoogleFontsList.description = 'Fetch and recreate the PHP file holding the Google Fonts list.';
+gulp.task( 'update-php-google-fonts-list', updatePhpGoogleFontsList  );
+
+
+// -----------------------------------------------------------------------------
+// Styles related
+// -----------------------------------------------------------------------------
 
 function logError( err, res ) {
-  log( c.red( 'Sass failed to compile' ) );
-  log( c.red( '> ' ) + err.file.split( '/' )[err.file.split( '/' ).length - 1] + ' ' + c.underline( 'line ' + err.line ) + ': ' + err.message );
+  log.error( 'Sass failed to compile' );
+  log.error( '> ' + err.file.split( '/' )[err.file.split( '/' ).length - 1] + ' ' + 'line ' + err.line + ': ' + err.message );
 }
+
+var log = require('fancy-log');
 
 function stylesDev () {
   return gulp.src(source_SCSS)
