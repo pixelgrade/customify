58003360d3ecb60048a535d21a9b398a4bded7a5
Vlad Olaru <vlad.olaru@pixelgrade.com>
2020-06-16 18:14:01 +0300

    Add notice to migrate theme_mods from parent to child - see #207

--- includes/extras.php
+++ includes/extras.php
@@ -322,3 +322,150 @@ if ( ! function_exists('add_customify_base_options') ) {
 	}
 }
 add_filter( 'customify_filter_fields', 'add_customify_base_options', 5, 1 );
+
+add_action( 'after_switch_theme', function( $old_theme_name, $old_theme ) {
+	$current_theme = wp_get_theme();
+	// If the current theme is a child theme, show a notice.
+	if ( $current_theme->exists()
+	     && $old_theme->exists()
+	     && $current_theme->get_template() === $old_theme->get_stylesheet() ) {
+		add_action( 'admin_notices', 'customify_child_theme_migrate_theme_mods_notice' );
+	}
+}, 100, 2 );
+
+/**
+ * Provide a notice allowing for theme mods migration from the parent theme to the current child theme.
+ */
+function customify_child_theme_migrate_theme_mods_notice() {
+	global $pagenow;
+
+	// We only show the notice on the themes dashboard, and if we are allowed to.
+	if ( 'themes.php' !== $pagenow
+	     || ! is_child_theme()
+	     || true !== apply_filters( 'customify_allow_child_theme_mod_migrate_notice', true )
+	     || ! current_user_can( 'manage_options' ) ) {
+
+		return;
+	}
+
+	$parent_theme = wp_get_theme( get_template() );
+	if ( ! $parent_theme->exists() ) {
+		return;
+	}
+
+	ob_start(); ?>
+	<div class="customify-notice__container updated notice fade is-dismissible">
+		<h3><?php echo sprintf( __( 'You have activated a child theme for "%s". Good for you!', 'customify' ), $parent_theme->get('Name') ); ?></h3>
+		<p>
+			<?php echo wp_kses_post( __( 'If you have already <strong>set up things in the Customizer,</strong> you may want to <strong>keep those customizations</strong> so you don\'t start over.', 'customify' ) ); ?>
+		</p>
+		<p>
+			<?php echo wp_kses_post( __( 'So, the question is simple: <strong>would you like to migrate all theme-specific options (theme mods) from the parent theme to the child one?</strong>', 'customify' ) ); ?>
+		</p>
+		<p>
+			<?php echo wp_kses_post( __( 'All parent theme customizations will remain in place, while those of the active child theme will be overwritten, if any.', 'customify' ) ); ?>
+		</p>
+		<form class="customify-notice-form" method="post">
+			<noscript><input type="hidden" name="customify-notice-no-js" value="1"/></noscript>
+
+			<p>
+				<button class="customify-notice-button button button-primary js-handle-customify">
+					<span class="customify-notice-button__text"><?php esc_html_e( 'Yes, migrate customizations', 'customify' ); ?></span>
+				</button>
+				<button type="submit" class="customify-dismiss-button button button-secondary js-dismiss-customify"><?php esc_html_e( 'No, thank you', 'customify' ); ?></button>
+				&nbsp;<span class="message js-plugin-message" style="font-style:italic"></span>
+			</p>
+
+			<?php wp_nonce_field( 'customify_migrate_customizations_from_parent_to_child_theme', 'nonce-customify_theme_mods_migrate' ); ?>
+		</form>
+	</div>
+	<script>
+		(function ($) {
+			$(function () {
+				var $noticeContainer = $('.customify-notice__container'),
+					$button = $noticeContainer.find('.js-handle-customify'),
+					$buttonText = $noticeContainer.find('.customify-notice-button__text'),
+					$dismissButton = $noticeContainer.find('.js-dismiss-customify'),
+					$statusMessage = $noticeContainer.find('.js-plugin-message')
+
+				$button.on('click', function (e) {
+					e.preventDefault();
+
+					$buttonText.html("<?php esc_html_e( 'Migrating customizations..' ,'customify'); ?>")
+					$button.attr('disabled', true)
+					$dismissButton.hide()
+
+					$.ajax({
+						url: "<?php echo admin_url( 'admin-ajax.php' ); ?>",
+						type: 'post',
+						data: {
+							action: 'customify_migrate_customizations_from_parent_to_child_theme',
+							nonce_migrate: $noticeContainer.find('#nonce-customify_theme_mods_migrate').val()
+						}
+					})
+					.done(function(response) {
+						if (typeof response.success !== 'undefined' && response.success) {
+							$statusMessage.html("<?php esc_html_e( 'Successfully migrated the parent customizations! Enjoy crafting your site!', 'customify' ); ?>")
+							$buttonText.html("<?php esc_html_e( 'Finished migration', 'customify' ); ?>")
+						} else {
+							$statusMessage.html("<?php esc_html_e( 'Something went wrong and we couldn\'t migrate the customizations.' ,'customify'); ?>")
+							$buttonText.html("<?php esc_html_e( 'Migration error' ,'customify'); ?>")
+						}
+					})
+					.fail(function() {
+						$statusMessage.html("<?php esc_html_e( 'Something went wrong and we couldn\'t migrate the customizations.' ,'customify'); ?>")
+						$buttonText.html("<?php esc_html_e( 'Migration error' ,'customify'); ?>")
+					})
+				})
+
+				$dismissButton.on('click', function (e) {
+					e.preventDefault();
+
+					$noticeContainer.slideUp();
+				})
+			})
+		})(jQuery)
+	</script>
+	<?php
+	echo ob_get_clean();
+}
+
+/**
+ * Process ajax call to migrate customizations from parent to current child theme.
+ */
+function customify_migrate_customizations_from_parent_to_child_theme() {
+	// Check nonce.
+	check_ajax_referer( 'customify_migrate_customizations_from_parent_to_child_theme', 'nonce_migrate' );
+
+	$parent_theme = wp_get_theme( get_template() );
+	if ( ! $parent_theme->exists() ) {
+		wp_send_json_error();
+	}
+
+	// Migrate theme mods
+	$parent_theme_mods = get_option( "theme_mods_" . $parent_theme->get_stylesheet() );
+	// We need to exclude certain theme_mods since they are not needed by the child theme.
+	$excluded = [
+		'pixcare_license',
+		'pixcare_new_theme_version',
+		'pixcare_install_notice_dismissed',
+	];
+	foreach ( $excluded as $exclude ) {
+		unset( $parent_theme_mods[ $exclude ] );
+	}
+	// Finally, write the new theme mods for the active child theme.
+	if ( ! update_option( "theme_mods_" . get_option( 'stylesheet' ), $parent_theme_mods ) ) {
+		wp_send_json_error( esc_html__( 'Could not update the child theme theme_mods.', 'customify' ) );
+	}
+
+	// Redirect if this is not an ajax request.
+	if ( isset( $_POST['pixcare-notice-no-js'] ) ) {
+
+		// Go back to where we came from.
+		wp_safe_redirect( wp_get_referer() );
+		exit();
+	}
+
+	wp_send_json_success();
+}
+add_action( 'wp_ajax_customify_migrate_customizations_from_parent_to_child_theme', 'customify_migrate_customizations_from_parent_to_child_theme' );
